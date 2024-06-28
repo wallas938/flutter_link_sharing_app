e8fb06e5d6b7fe5c1cc25345b8014cf5007705be
wallas938 <drame.sissako001@gmail.com>
2024-06-28 18:02:41 +0200

    finir login ecran

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_link_sharing_app/app_constants/theme.colors.dart';
+import 'package:flutter_link_sharing_app/app_constants/theme.typo.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -10,9 +14,15 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return const MaterialApp(
+    return MaterialApp(
       title: 'Link Sharing App',
-      home: LoginScreen(),
+      theme: ThemeData(
+          textTheme: TextTheme(
+              bodySmall: AppTypography.bodyS,
+              bodyMedium: AppTypography.bodyM,
+              headlineSmall: AppTypography.headingS,
+              headlineMedium: AppTypography.headingM)),
+      home: const LoginScreen(),
     );
   }
 }
@@ -28,7 +38,42 @@ class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return const Scaffold();
+    return Scaffold(
+      backgroundColor: Theme.of(context).primaryColor,
+      appBar: AppBar(
+        backgroundColor: AppColors.red,
+        title: const Text("LINK SHARING APP"),
+      ),
+      body: Expanded(
+        flex: 1,
+        child: Container(
+          color: Colors.redAccent,
+          padding: const EdgeInsets.all(32.0),
+          margin: const EdgeInsets.fromLTRB(0, 0, 0, 64),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              Expanded(
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    SizedBox(
+                      child: SvgPicture.asset(
+                        'assets/images/logo-devlinks-small.svg',
+                        width: 40,
+                      ),
+                    ),
+                    Text("devlinks", style: AppTypography.headingM)
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+      )
+    );
   }
 }
 
