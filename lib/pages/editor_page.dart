e13139433538c6849b694f33ec757f38613ab6fa
wallas938 <drame.sissako001@gmail.com>
2024-07-09 17:42:35 +0200

    trouver logique pour mise a jour des couleurs de chaque tab

--- lib/pages/editor_page.dart
+++ lib/pages/editor_page.dart
@@ -14,75 +14,80 @@ class _EditorPageState extends State<EditorPage> {
   Widget build(BuildContext context) {
     return MaterialApp(
         home: DefaultTabController(
-      length: 4,
+      length: 2,
       child: Scaffold(
         appBar: AppBar(
+          backgroundColor: Colors.purple,
           elevation: 0,
-          bottom: TabBar(
-            padding: const EdgeInsets.fromLTRB(24, 16, 16, 16),
-            physics: const BouncingScrollPhysics(),
-            unselectedLabelColor: AppColors.grey,
-            dividerHeight: 0,
-            tabAlignment: TabAlignment.center,
-            // ICI
-            indicator: const BoxDecoration(),
-            labelColor: Colors.orange,
-            labelStyle:
-                const TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),
-            tabs: [
-              SizedBox(
-                child: SvgPicture.asset(
-                  'assets/images/logo-devlinks-small.svg',
-                ),
-              ), // LOGO_TAB
-              Container(
-                decoration: BoxDecoration(
-                    color: AppColors.lightPurple,
-                    borderRadius: BorderRadius.circular(8)),
-                child: Padding(
-                  padding:
-                      const EdgeInsets.symmetric(vertical: 11, horizontal: 27),
+          bottom: PreferredSize(
+            preferredSize: const Size.fromHeight(42),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                SizedBox(
                   child: SvgPicture.asset(
-                    'assets/images/icon-link.svg',
-                    colorFilter: const ColorFilter.mode(
-                        AppColors.purple, BlendMode.srcIn),
+                    'assets/images/logo-devlinks-small.svg',
                   ),
                 ),
-              ), // LINK_TAB
-              Container(
-                height: 42,
-                decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(8),
-                    color: Colors.amber),
-                child: Padding(
-                  padding:
-                      const EdgeInsets.symmetric(vertical: 11, horizontal: 27),
-                  child: SvgPicture.asset(
-                    'assets/images/icon-profile-header.svg',
-                  ),
+                TabBar(
+                  indicatorPadding: EdgeInsets.zero,
+                  labelPadding: EdgeInsets.zero,
+                  unselectedLabelColor: AppColors.grey,
+                  indicatorColor: Colors.red,
+                  isScrollable: true,
+                  dividerHeight: 0,
+                  tabAlignment: TabAlignment.start,
+                  indicator: const BoxDecoration(),
+                  labelColor: Colors.orange,
+                  labelStyle: const TextStyle(
+                      fontSize: 18.0, fontWeight: FontWeight.bold),
+                  tabs: [
+                    // LOGO_TAB
+                    Container(
+                      decoration: BoxDecoration(
+                          color: AppColors.lightPurple,
+                          borderRadius: BorderRadius.circular(8)),
+                      child: Container(
+                        padding: const EdgeInsets.symmetric(vertical: 11, horizontal: 27),
+                        child: SvgPicture.asset(
+                          'assets/images/icon-link.svg',
+                          colorFilter: const ColorFilter.mode(
+                              AppColors.purple, BlendMode.srcIn),
+                        ),
+                      ),
+                    ), // LINK_TAB
+                    Container(
+                      height: 42,
+                      padding: const EdgeInsets.symmetric(vertical: 11, horizontal: 27),
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(8),
+                          color: Colors.amber),
+                      child: SvgPicture.asset(
+                        'assets/images/icon-profile-header.svg',
+                      ),
+                    ), // PROFILE_TAB
+                  ],
                 ),
-              ), // PROFILE_TAB
-              Container(
-                width: 74,
-                height: 42,
-                decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(8),
-                    border: Border.all(width: 1.0, color: AppColors.purple)),
-                child: Padding(
-                  padding:
-                      const EdgeInsets.symmetric(vertical: 11, horizontal: 16),
-                  child: SvgPicture.asset(
-                    'assets/images/icon-preview-header.svg',
+                Container(
+                  width: 52,
+                  height: 42,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(8),
+                      border: Border.all(width: 1.0, color: AppColors.purple)),
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(
+                        vertical: 11, horizontal: 16),
+                    child: SvgPicture.asset(
+                      'assets/images/icon-preview-header.svg',
+                    ),
                   ),
-                ),
-              ), // PREVIEW_TAB
-            ],
+                ), // PREVIEW_TAB
+              ],
+            ),
           ),
         ),
         body: const TabBarView(
           children: [
-            Icon(Icons.directions_car),
-            Icon(Icons.directions_car),
             Icon(Icons.directions_car),
             Icon(Icons.directions_car),
           ],
